efe38c693db1a8236311335bfeca1dab4855ef68
kalifa <adkalifa@gmail.com>
2020-03-19 19:53:23 +0530

    login page update

---
+++ src/Component/table/index.js
@@ -0,0 +1,26 @@
+
+import React, { Component } from 'react';
+import style from '../../Assets/style.css';
+import '../../../src/App.css';
+import HeaderPage from '../Header';
+
+class LoginPage extends Component {
+    
+    handleClick = () => {
+        window.location.assign(HeaderPage);
+        console.loh(window.location.assign(HeaderPage));
+    }
+render() {
+    return (
+        <div className="App">
+        <header className="login">
+            <input className="username" placeholder="Username" />
+            <input className="username" placeholder="Password" />
+            <button type="button" className="submit" onClick={this.handleClick}>Login</button>
+        </header>
+        </div>
+    );
+    }
+}
+
+export default LoginPage;
